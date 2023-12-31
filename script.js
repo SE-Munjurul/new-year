6ed988957f0baf79c793211f5a4812916c1017f1
SE-Munjurul <122313151+SE-Munjurul@users.noreply.github.com>
2024-01-01 07:38:33 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const greeting = document.querySelector('.greeting');
+    greeting.style.opacity = '1';
+    greeting.style.transform = 'translateY(0)';
+});
